581b4c057ba31514bb9a4996cd6e56a6ef530fe3
weldonyang <51804539+weldonyang@users.noreply.github.com>
2021-01-13 16:34:18 -0800

    Created DTR model and visualized it

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -1,4 +1,6 @@
 # Decision Tree Regression
+# NOTE: DTR No need for feature scaling
+# NOTE: DTR is not good at low feature datasets; works a lot better on datasets with many features 
 
 # Importing the libraries
 import numpy as np
@@ -11,19 +13,19 @@ X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
 # Training the Decision Tree Regression model on the whole dataset
-from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
+from sklearn.tree import DecisionTreeRegressor 
+regressor = DecisionTreeRegressor(random_state=0)
 regressor.fit(X, y)
 
 # Predicting a new result
-regressor.predict([[6.5]])
+print(regressor.predict([[6.5]]))
 
 # Visualising the Decision Tree Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
-plt.xlabel('Position level')
+X_grid = np.arange(min(X), max(X), 0.1)                                               
+X_grid = X_grid.reshape((len(X_grid), 1))   
+plt.scatter(X, y, color='red')                       
+plt.plot(X_grid, regressor.predict(X_grid), color='blue') 
+plt.title('Salary vs Position Level (DTR)')
+plt.xlabel('Postion Level')
 plt.ylabel('Salary')
 plt.show()
